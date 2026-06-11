984e28cbe0eddf6bc054336c1f799ab2566c55c2
sonbui <thaisonb936@gmail.com>
2026-06-11 21:40:59 +0700

    fix: validate booking request

--- src/main/java/com/example/project/model/dto/request/BookingRequest.java
+++ src/main/java/com/example/project/model/dto/request/BookingRequest.java
@@ -1,7 +1,7 @@
 package com.example.project.model.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,10 +17,10 @@ import java.time.LocalDateTime;
 public class BookingRequest {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     @NotNull(message = "Thời gian bắt đầu không được để để trống")
-    @Future(message = "Thời gian bắt đầu phải là trong tương lai")
+    @FutureOrPresent(message = "Thời gian bắt đầu phải là trong tương lai")
     private LocalDateTime startTime;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     @NotNull(message = "Thời gian kết thúc không được để trống")
-    @Future(message = "Thời gian kết thúc phải là ngày trong tương lai")
+    @FutureOrPresent(message = "Thời gian kết thúc phải là ngày trong tương lai")
     private LocalDateTime endTime;
 }
